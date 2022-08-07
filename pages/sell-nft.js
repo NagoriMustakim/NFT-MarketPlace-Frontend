f03da2e7e6c5512bedb0dd95e9713f0686b0b688
root <root@LAPTOP-P07JSRR3.localdomain>
2022-08-07 20:38:03 +0530

    NFT-MarketPlace-Frontend initial commit

---
+++ pages/sell-nft.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>NFT MarketPlace || Buy || Sell</title>
+                <meta name="description" content="NFT MarketPlace Buy and sell your NFT" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            sell page
+        </div>
+    )
+}
